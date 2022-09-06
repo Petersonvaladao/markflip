a80bfc3350f9f33eb0782e1c9ad8f87bc70c0b73
Peterson Valadão <73299678+Petersonvaladao@users.noreply.github.com>
2022-09-06 10:57:44 -0300

    add scroll reveal

--- main.js
+++ main.js
@@ -1,19 +1,40 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+// Scroll to section
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
   anchor.addEventListener('click', function (e) {
-      e.preventDefault();
+    e.preventDefault();
 
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-          behavior: 'smooth'
-      });
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth',
+    });
   });
 });
 
-
 function myFunction() {
-  var x = document.querySelector(".nav-wrapper");
-  if (x.style.display === "block") {
-    x.style.display = "none";
+  var x = document.querySelector('.nav-wrapper');
+  if (x.style.display === 'block') {
+    x.style.display = 'none';
   } else {
-    x.style.display = "block";
+    x.style.display = 'block';
   }
 }
+
+// fade in
+// const observer = new IntersectionObserver(
+//   (entries) => {
+//     Array.from(entries).forEach((entry) => {
+//       if (entry.intersectionRatio >= 1) {
+//         entry.target.classList.add('fade-off');
+//       }
+//     });
+//   },
+//   {
+//     threshold: [0, 0.5, 1]
+//   }
+// );
+
+// Array.from(document.querySelectorAll('.fade-in')).forEach((element) => {
+//   observer.observe(element);
+// });
+
+window.sr = ScrollReveal({ reset: true });
+sr.reveal('.fade-in', { duration: 1000 });
